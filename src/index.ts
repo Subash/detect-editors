02619a9fce05e4d795a38b5b9e85adbce3969cd0
Subash Pathak <Subash@users.noreply.github.com>
2022-08-08 20:44:33 +0545

    Types

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { getAvailableEditors, launchExternalEditor } from './editors/index';
+import { FoundEditor } from './editors/shared';
 
-async function launchEditor(editor: any, fullPath: any) {
+async function launchEditor(editor: FoundEditor, fullPath: string) {
   return await launchExternalEditor(fullPath, editor);
 }
 
